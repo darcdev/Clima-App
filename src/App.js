585704105619a52e076599f33d64446f20c1117c
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-11-19 13:01:52 -0500

    consultando api clima

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
+import Clima from "./components/Clima";
 function App() {
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = "53fcb144cc1c53ab21edaae7c6ec7fc4";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await (await fetch(url)).json();
+        guardarResultado(respuesta);
+        guardarConsultar(false);
+      }
+    };
+
+    consultarAPI();
+  }, [consultar]);
   return (
     <>
       <Header titulo="Clima App" />
@@ -10,9 +33,15 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
-            <div className="col m6 s12">2</div>
+            <div className="col m6 s12">
+              <Clima resultado={resultado} />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Formulario = () => {
+const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
+  const [error, guardarError] = useState(false);
+  // extraer ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  // colocar datos en el state
+
+  const handleChange = (evt) => {
+    // actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  // enviar datos del formulario
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (ciudad.trim() === "" || pais.trim() === "") {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+    guardarConsultar(true);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      ) : null}
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
         <label htmlFor="ciudad">Ciudad:</label>
       </div>
       <div className="input-field col s12">
-        <select name="pais" id="pais">
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
           <option value="">-- Seleccione un país --</option>
           <option value="US">Estados Unidos</option>
           <option value="MX">México</option>
@@ -19,6 +55,15 @@ const Formulario = () => {
           <option value="PE">Perú</option>
         </select>
         <label htmlFor="pais">País</label>
+      </div>
+      <div className="input-field col s12">
+        <button
+          type="submit"
+          value="Buscar Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 col s12"
+        >
+          Buscar CLima
+        </button>
       </div>
     </form>
   );
